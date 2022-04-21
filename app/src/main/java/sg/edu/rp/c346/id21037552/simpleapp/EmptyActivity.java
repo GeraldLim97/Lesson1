b208b28d9b70af8d2643ce0759019960a8d08ce2
GeraldLim97 <20137552@myrp.edu.sg>
2022-04-21 15:39:08 +0800

    Lesson1

---
+++ app/src/main/java/sg/edu/rp/c346/id21037552/simpleapp/EmptyActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id21037552.simpleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simpleapp);
+    }
+}
